7e09d6ae14585ce71883d98de8d2d36d279143df
Lanay Brown <lanayb.03@gmail.com>
2022-02-20 20:13:13 -0500

    made tables and seeds

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const sequelize = require('./config/connection');
+const models = require('./models');
+
 
 const app = express();
 

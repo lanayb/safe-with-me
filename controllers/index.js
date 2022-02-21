868070923a2102b3435f6c69f374088c49200e25
Lanay Brown <lanayb.03@gmail.com>
2022-02-20 21:54:45 -0500

    made handlebars, got handlebars to work

--- controllers/index.js
+++ controllers/index.js
@@ -4,15 +4,15 @@ const { User } = require('../models');
 router.get('/', async(req, res) => {
     try {
         const userData = await User.findAll();
-        res.status(200).json(userData);
+        // res.status(200).json(userData);
 
-        const serializedUser = userData.map
-        (user => user.get({plain: true}));
+        const serializedUser = userData.map(
+        (user) => user.get({ plain: true}));
         console.log(serializedUser);
-        res.render('confess');
 
+        res.render('confess', { serializedUser });
     } catch(err) {
-        return res.status(500).json(err);
+         res.status(500).json(err);
     }
 });
 

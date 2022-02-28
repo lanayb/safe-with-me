1ac50e44c1993298682fa665090bcd7e152162d2
Lanay Brown <lanayb.03@gmail.com>
2022-02-27 23:10:26 -0500

    added session

--- controllers/api/homepage.js
+++ controllers/api/homepage.js
@@ -11,10 +11,20 @@ router.get('/entry', async(req, res) => {
     }
 });
 
+// /api/homepage/newEntry
 router.post('/newEntry', async(req, res) => {
     try {
-        const newEntry = await Confession.create(req.body);
-        res.status(200).json(newEntry);
+        const newEntry = await Confession.create({
+            description: req.body.description,
+            user_id: req.body.user_id,
+        });
+
+        req.session.save(() => {
+            req.session.loggedIn = true;
+
+            res.status(200).json(newEntry);
+        });
+        
     } catch(err) {
         res.status(400).json(err);
     }
--- controllers/api/login.js
+++ controllers/api/login.js
@@ -11,14 +11,55 @@ router.get('/userLogin', async(req, res) => {
     }
 });
 
+// /api/login/userLogin
 router.post('/userLogin', async(req, res) => {
     try {
-        const userLogin = await User.create(req.body);
-        res.status(200).json(userLogin);
+        const userLogin = await User.create({
+            where: {
+                user_name: req.body.user_name,
+                password: req.body.password,
+            },
+        });
+
+        if(!userLogin) {
+            res.status(400).json({
+                message: 'Wrong email or password. Try again please!'
+            });
+        }
+
+        const validPW = await userLogin.checkPassword(req.body.password);
+
+        if(!validPW) {
+            res.status(400).json({
+                message: 'Wrong email or password. Try again please!'
+            });
+        }
+
+        req.session.save(() => {
+            req.session.loggedIn = true;
+
+            res.status(200).json({
+                user: userLogin, 
+                message: 'Nice, you are now logged in!'
+            });
+        });
+
     } catch(err) {
         res.status(400).json(err);
     }
-    
+});
+
+
+// logout route
+// api/login/logout
+router.post('/logout', (req, res) => {
+    if(req.sessiom.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
 });
 
 module.exports = router;

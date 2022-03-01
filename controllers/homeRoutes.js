355aa0c13453d0eb269e81a6c4e01257074ce23c
Lanay Brown <lanayb.03@gmail.com>
2022-02-28 20:19:30 -0500

    connected backend to front end

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,28 +3,50 @@ const { User, Confession } = require("../models");
 const path = require('path');
 
 
-router.get('/home', async(req, res) => {
-    try {
-        res.render('homePage', {});
-    } catch(err) {
-        res.status(400).json(err);
-    }
+// landing page
+router.get('/', async(req,res) => {
+    const conData = await Confession.findAll().catch((err) => {
+        res.json(err);
+    });
+    const confess = conData.map((confess) => confess.get({ plain:true}));
+    res.render('confess', {confess});
 });
 
+
+// signup page
 router.get('/signup', async(req, res) => {
-    try{
-        res.render('signUp', {});
-    } catch(err) {
-        res.status(400).json(err);
-    }
+   const signupPg = await User.findAll().catch((err) => {
+        res.json(err);
+   });
+   const signup = signupPg.map((signup) => signup.get({ plain:true }));
+   res.render('signup', {signup});
 });
 
+
+// login
 router.get('/login', async(req, res) => {
-    try{
-        res.render('login', {});
-    } catch(err) {
-        res.status(400).json(err);
-    }
+    const loginPg = await User.findAll().catch((err) => {
+        res.json(err);
+   });
+   const login = loginPg.map((login) => login.get({ plain:true }));
+   res.render('login', {login});
 });
+
+// homepage
+router.get('/homepage', async(req,res) => {
+    const home = await User.findAll().catch((err) => {
+        res.json(err);
+    });
+    const homepage = home.map((homepage) => homepage.get({ plain:true}));
+    res.render('homepage', {homepage});
+});
+
 
 module.exports = router;
+
+//    const userSignup =  await User.findAll().catch((err) => {
+//         res.json(err);
+//    });
+
+//    const signup = userSignup.map((signup) => signup.get({ plain:true}));
+//    res.render('signup', {signup});
